d88739fec03a87b237c93498d7cab8ec5bdb23c3
rizalaji1st <rizalaji561@gmail.com>
2021-06-19 12:37:41 +0700

    Menambahkan bottom tab screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './screens/tabs_screen.dart';
 import './screens/meal_detail_screen.dart';
 import './screens/category_meals_screen.dart';
 
@@ -37,7 +38,7 @@ class MyApp extends StatelessWidget {
       //home: CategoriesScreen(),
       initialRoute: '/',
       routes:{
-        '/': (ctx) => CategoriesScreen(),
+        '/': (ctx) => TabsScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import './favorites_screen.dart';
+import './categories_screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({Key? key}) : super(key: key);
+
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = [
+    {'title': 'Categories', 'page': CategoriesScreen() ,},
+    { 'title': 'Your Favorites','page': FavoritesScreen(),},
+  ];
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(_pages[_selectedPageIndex]['title'] as String),
+          ),
+          body: _pages[_selectedPageIndex]['page'] as Widget,
+          bottomNavigationBar: BottomNavigationBar(
+            onTap: _selectPage,
+            backgroundColor: Theme.of(context).primaryColor,
+            unselectedItemColor: Colors.white,
+            selectedItemColor: Theme.of(context).accentColor,
+            currentIndex: _selectedPageIndex,
+            items: [
+              BottomNavigationBarItem(
+                backgroundColor: Theme.of(context).primaryColor,
+                icon: Icon(Icons.category),
+                // ignore: deprecated_member_use
+                title: Text('Categories'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.star),
+                // ignore: deprecated_member_use
+                title: Text('Favorites'),
+              )
+            ],
+          ),
+        ));
+  }
+}
