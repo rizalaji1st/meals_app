4f479a5b4d530a40d7147eb66766b9d6e3249d28
rizalaji1st <rizalaji561@gmail.com>
2021-06-21 21:31:06 +0700

    Menambahkan fitur favorit

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -2,20 +2,28 @@ import 'package:flutter/material.dart';
 import '../widgets/main_drawer.dart';
 import './favorites_screen.dart';
 import './categories_screen.dart';
+import '../models/meal.dart';
 
 class TabsScreen extends StatefulWidget {
-  const TabsScreen({Key? key}) : super(key: key);
+  final List<Meal> favoriteMeal;
+  TabsScreen(this.favoriteMeal);
 
   @override
   _TabsScreenState createState() => _TabsScreenState();
 }
 
 class _TabsScreenState extends State<TabsScreen> {
-  final List<Map<String, Object>> _pages = [
-    {'title': 'Categories', 'page': CategoriesScreen() ,},
-    { 'title': 'Your Favorites','page': FavoritesScreen(),},
-  ];
+  late List<Map<String, Object>> _pages;
   int _selectedPageIndex = 0;
+
+  @override
+  void initState() {
+    _pages = [
+    {'title': 'Categories', 'page': CategoriesScreen() ,},
+    { 'title': 'Your Favorites','page': FavoritesScreen(widget.favoriteMeal),},
+  ] ;
+    super.initState();
+  }
 
   void _selectPage(int index) {
     setState(() {
